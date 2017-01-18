2d32d10f2dd56ed8b85caec89ed4e0399c86870b
Waynan <Reefdude@gmail.com>
2017-01-18 13:14:25 +0200

    Create ESPCONTROL.php

---
+++ PHP/ESPCONTROL.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$light = $_GET['light'];
+if($light == "on") {
+  $file = fopen("light.json", "w") or die("can't open file");
+  fwrite($file, '{"light": "on"}');
+  fclose($file);
+} 
+else if ($light == "off") {
+  $file = fopen("light.json", "w") or die("can't open file");
+  fwrite($file, '{"light": "off"}');
+  fclose($file);
+}
+?>
+
+<html>
+  <head>      
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>ESP8266</title>
+   
+</html>
